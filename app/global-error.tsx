415aaa6e5fdf55e49a38119398143530bbf68d5e
Jason Au <jasontau@gmail.com>
2023-10-23 19:41:00 -0700

    navigation, routing, error handling

---
+++ app/global-error.tsx
@@ -0,0 +1,11 @@
+// captures all errors including in app layout
+'use client'
+import React from 'react'
+
+const GlobalError = () => {
+  return (
+    <div>An unexpected global error has occurred.</div>
+  )
+}
+
+export default GlobalError
