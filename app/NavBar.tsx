777ed50a31ec1adf4f8383a44f300f10e86ce0ac
Jason Au <jasontau@gmail.com>
2023-11-12 23:34:31 -0800

    add middleware and protected routes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,13 +7,15 @@ import { useSession } from 'next-auth/react'
 const NavBar = () => {
   const {status, data: session } = useSession();
 
-  if (status === 'loading') return null;
-
   return (
     <div className='flex bg-slate-200 p-5 space-x-3'>
       <Link href="/" className="mr-5">Next.js</Link>
       <Link href="/users">Users</Link>
-      {status === 'authenticated' && <div>{session.user!.name}</div>}
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'authenticated' && <div>
+        {session.user!.name}
+        <Link className="ml-3" href="/api/auth/signout">Sign Out</Link>
+      </div>}
       {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
     </div>
   )
